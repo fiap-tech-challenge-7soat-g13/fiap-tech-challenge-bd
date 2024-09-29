4967cb0e77f2c1044bca1a8b2eb0e6383df6fda6
Cristiano de Barros <cristiano.barros13@gmail.comls>
2024-09-29 20:50:22 -0300

    Ajustes

--- main.tf
+++ main.tf
@@ -15,22 +15,22 @@ resource "aws_security_group" "default" {
   vpc_id = data.aws_vpc.default.id
   name   = "security-group"
   ingress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 resource "aws_db_instance" "default" {
-  identifier          = "database-1"
-  instance_class      = "db.t4g.micro"
-  allocated_storage   = 20
-  engine              = "postgres"
-  engine_version      = "16.3"
-  username            = var.db_username
-  password            = var.db_password
-  db_name             = var.db_name
-  publicly_accessible = true
-  skip_final_snapshot = true
+  identifier             = "database-1"
+  instance_class         = "db.t4g.micro"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "16.3"
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = var.db_name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.default.id]
 }
